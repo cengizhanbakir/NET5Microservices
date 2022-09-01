d634a9fb49a247eb3c5d95c21ef7f0f28f2994d6
Cengizhan BAKIR <cengizhanbakir@gmail.com>
2022-09-01 23:05:39 +0300

    Added CustomBaseController class for status codes. Eg : return Ok().

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using FreeCourse.Services.Catalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CoursesController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+
+    }
+}
