61fc26f8868aec298536ea51e4130b5877dc0b9d
Cengizhan BAKIR <cengizhanbakir@gmail.com>
2022-09-01 22:19:29 +0300

    Added GetAllAsync method for Courses.

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -14,6 +14,8 @@ namespace FreeCourse.Services.Catalog.Services
     internal class CourseService
     {
         private readonly IMongoCollection<Course> _courseCollection;
+        private readonly IMongoCollection<Category> _categoryCollection;
+
         private readonly IMapper _mapper;
 
         public CourseService(IMapper mapper, IDatabaseSettings databaseSettings)
@@ -21,13 +23,31 @@ namespace FreeCourse.Services.Catalog.Services
 
             var client = new MongoClient(databaseSettings.ConnectionString);
             var database = client.GetDatabase(databaseSettings.DatabaseName);
+
             _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+
             _mapper = mapper;
 
         }
 
         public async Task<Response<List<CourseDto>>> GetAllAsync()
         {
+            var courses = await _courseCollection.Find(course => true).ToListAsync();
+
+            if (courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category = await _categoryCollection.Find<Category>(x => x.Id == course.CategoryId).FirstAsync();
+                }
+            }
+            else
+            {
+                courses = new List<Course>();
+            }
+
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
 
         }
     }
