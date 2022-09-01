6924fe8706dd9f9bd7b4ac7a3478dd6362c2c981
Cengizhan BAKIR <cengizhanbakir@gmail.com>
2022-09-01 22:10:52 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.Dtos;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    internal class CourseService
+    {
+        private readonly IMongoCollection<Course> _courseCollection;
+        private readonly IMapper _mapper;
+
+        public CourseService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _mapper = mapper;
+
+        }
+
+        public async Task<Response<List<CourseDto>>> GetAllAsync()
+        {
+
+        }
+    }
+}
